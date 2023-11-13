cb1fd093b1f71378c860ce03d2ca084aa7c96af1
NoahMey <noah.my@hotmail.com>
2023-11-13 19:14:23 +0100

    add flatten A6.6

--- uebungsblätter/Blatt 6/Flatten.java
+++ uebungsblätter/Blatt 6/Flatten.java
@@ -1,19 +1,28 @@
 public class Flatten {
     // Ihre Lösung kommt hier hin
     public static int[] flatten(int[][] a){
+        int count = 0;
         for(int x = 0; x<a.length; x++){
             for(int elem : a[x]){
                 System.out.println(elem);
+                count += 1;
             }
         }
-        int[] na = new int[3];
+        int[] na = new int[count];
+        int index = 0;
+        for(int x = 0; x<a.length; x++){
+            for(int elem : a[x]){
+                na[index] = elem;
+                index += 1;
+            }
+        }
         return na;
     }
     public static void main(String[] args) {
         System.out.println(java.util.Arrays.toString(
             flatten(new int[][]{
                 {1,2},
-                {0},
+                {},
                 {3,4,5,6},
                 {7},
                 {8,9,10}
