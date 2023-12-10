91e4aca779f0c7c441b7c2b21a51289bbc69acad
NoahMey <noah.my@hotmail.com>
2023-12-10 23:40:08 +0100

    generic list

--- uebungsblätter/Blatt_10/uebung010/src/main/java/generics/Main.java
+++ uebungsblätter/Blatt_10/uebung010/src/main/java/generics/Main.java
@@ -1,9 +1,27 @@
-
 package generics;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        // Testen Sie hier Ihre Implementation
+        int len = 6;
+        Integer[] testArray = new Integer[len];
+        for(int index = 0; index < len/2; index++){
+            testArray[index] = index;
+        }
+        
+        for(int index = len-1; index > len/2; index--){
+            testArray[index] = index-len/2;
+        }
+
+        for (int num : testArray) {
+            System.out.print(num + " ");
+        }
+
+        SortedList <Integer> sortlist = new SortedList<>(testArray);
+        for(int index = 0; index < sortlist.size; index++){
+            System.out.println(sortlist.get(index));
+        }
+        
     }
 }
--- uebungsblätter/Blatt_10/uebung010/src/main/java/generics/SortedList.java
+++ uebungsblätter/Blatt_10/uebung010/src/main/java/generics/SortedList.java
@@ -1,3 +1,48 @@
 package generics;
+import java.util.*;
 
+public class SortedList <T extends Comparable<T>>{
+
+    T[] testlist;
+    int size;
+    public SortedList(T[] testlist){
+        this.size = testlist.length;
+        this.testlist = sortiere(testlist);
+        
+    }
+
+    public T[] sortiere(T[] testlist){
+        T[] nl = testlist;
+        T curMin = testlist[0];
+        T maxV = testlist[0];
+        for(T element: testlist){
+                if (curMin.compareTo(element) > 0) {
+                    maxV = element;
+                }
+            }
+        for (int nlIndex= 0; nlIndex < size; nlIndex++ ){
+            int currentIndex = 0;
+            int replaceIndex = 0;
+            for(; replaceIndex < size; replaceIndex++){
+                if (curMin.compareTo(testlist[replaceIndex]) < 0) {
+                    curMin = testlist[replaceIndex];
+                    currentIndex = replaceIndex;
+                }
+            }
+            nl[nlIndex] = curMin;
+            testlist[currentIndex] = maxV;
+        }
+        return nl;
+    }
+
+    public T get(int index){
+        try {
+            return testlist[index];
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+        return null; //weshalb muss ich dies tun?
+    }
+
+}
 // Hier kommt Ihre Implementation der Klasse SortedList hin
