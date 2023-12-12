167693b05a6baf58d39433c2f664d6feffec5d6e
NoahMey <noah.my@hotmail.com>
2023-12-12 16:55:32 +0100

    add Sortlist size

--- uebungsblätter/Blatt_10/uebung010/src/main/java/generics/Main.java
+++ uebungsblätter/Blatt_10/uebung010/src/main/java/generics/Main.java
@@ -4,24 +4,35 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        int len = 6;
-        Integer[] testArray = new Integer[len];
-        for(int index = 0; index < len/2; index++){
-            testArray[index] = index;
-        }
+        NodeList<Integer> testLinked = new NodeList<>();
+        testLinked.addNode(42);
+        testLinked.addNode(11);
+        testLinked.addNode(23);
+        testLinked.printLinkedList();
+        SortedList<Integer> sorList = new SortedList<>(testLinked); //Eig. bräuchte ich hier erst generics mit comparable, da ich hier erst sortieren möchte.
+        System.out.println(sorList.getSize());
         
-        for(int index = len-1; index > len/2; index--){
-            testArray[index] = index-len/2;
-        }
+        
+        
+        
+        // int len = 6;
+        // Integer[] testArray = new Integer[len];
+        // for(int index = 0; index < len/2; index++){
+        //     testArray[index] = index;
+        // }
+        
+        // for(int index = len-1; index > len/2; index--){
+        //     testArray[index] = index-len/2;
+        // }
 
-        for (int num : testArray) {
-            System.out.print(num + " ");
-        }
+        // for (int num : testArray) {
+        //     System.out.print(num + " ");
+        // }
 
-        SortedList <Integer> sortlist = new SortedList<>(testArray);
-        for(int index = 0; index < sortlist.size; index++){
-            System.out.println(sortlist.get(index));
-        }
+        // SortedList <Integer> sortlist = new SortedList<>(testArray);
+        // for(int index = 0; index < sortlist.size; index++){
+        //     System.out.println(sortlist.get(index));
+        // }
         
     }
 }
--- uebungsblätter/Blatt_10/uebung010/src/main/java/generics/NodeList.java
+++ uebungsblätter/Blatt_10/uebung010/src/main/java/generics/NodeList.java
@@ -1,24 +1,48 @@
 package generics;
 
 public class NodeList<T extends Comparable<T>> {
-    
+
     Node<T> firstElement;
 
-    public NodeList(){
+    public NodeList() {
         firstElement = null;
     }
 
-    public Node<T> getLastNode(){
+    public Node<T> getLastNode() {
         Node<T> currentNode = firstElement;
-        while(currentNode.giveNextNode() != null){
+        //in case the nodelist is empty
+        if (currentNode == null) {
+            return null;
+        }
+        //while not pointer to next element = null= go further in the path, else give the node with node.givnextnode = null.
+        while (currentNode.giveNextNode() != null) {
             currentNode = currentNode.giveNextNode();
         }
         return currentNode;
     }
+    //adds a nodeto the last position, meaning where pointer is null set to the new node. if the list is empty, we initialize the first element to be the new node.
+    public void addNode(T something) {
+        Node<T> newNode = new Node<T>(something, null);
+        if (firstElement == null) {
+            firstElement = newNode;
+        } else {
+            getLastNode().setNextNode(newNode);
+        }
 
-    public void add(Node<T> newNode){
-        //
     }
 
+    public void printLinkedList(){
+        Node<T> currentNode = firstElement;
+        //in case the nodelist is empty, print empty
+        if (currentNode == null) {
+            System.out.println("The linked List is empty");
+        }
+        //while not pointer to next element = null= go further in the path, else give the node with node.givnextnode = null.
+        while (currentNode != null) {
+            System.out.println(currentNode.content);
+            currentNode = currentNode.giveNextNode();
+        }
+        return;
+    }
 
 }
