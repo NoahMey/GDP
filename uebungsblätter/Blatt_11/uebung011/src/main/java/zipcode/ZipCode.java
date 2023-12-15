d8e22c88e5904b27a6d5bf6d6c87dcd40bdaa5f1
NoahMey <noah.my@hotmail.com>
2023-12-15 13:18:42 +0100

    zipCode Complete

--- uebungsblätter/Blatt_11/uebung011/src/main/java/zipcode/ZipCode.java
+++ uebungsblätter/Blatt_11/uebung011/src/main/java/zipcode/ZipCode.java
@@ -9,7 +9,7 @@ import javax.tools.FileObject;
 public class ZipCode {
 
     Map<Integer, String> zipCodeMap = new HashMap<>();
-    Map<String, LinkedList<String>> nameWithCantonsMap = new HashMap<>();
+    Map<String, LinkedList<String>> nameWithCantonsMap = new HashMap<>(); // das genau anschauen, dann ist auch klar, was gebraucht wird!
 
     /**
      * Kreiert ein neues Zip-Code Objekt mit den in der Datei file übergebenen
@@ -24,8 +24,19 @@ public class ZipCode {
                 zipCodeMap.put(Integer.parseInt(splited[2]), splited[1]); //(int oder Integer geht nicht?)splited[2] typecast mit (type)
 
                 //nameWithCantonsMap.put(splited[1], splited[0]);
-                nameWithCantonsMap.put(splited[1], new LinkedList<>().add(splited[0]));
+                if (nameWithCantonsMap.containsKey(splited[1])) {
+                    LinkedList<String> temp = nameWithCantonsMap.get(splited[1]);
+                    temp.addLast(splited[0]); 
+                    nameWithCantonsMap.put(splited[1], temp);                
+                }else{
+                    LinkedList<String> temp = new LinkedList<String>(); 
+                    temp.addLast(splited[0]);
+                    nameWithCantonsMap.put(splited[1], temp);
+                }
+                
+                //nameWithCantonsMap.put(splited[1], new LinkedList<>().add(splited[0]));
             }
+            scanObj.close();
         } catch (Exception e) {
             throw e;
         }
@@ -37,9 +48,11 @@ public class ZipCode {
      * Postleitzahl nicht existiert, wird eine NoSuchElementException geworfen
      */
     String getNameForZipCode(int plz) throws NoSuchElementException {
-
-        // Ihre Implementation
-        return "";
+        if (zipCodeMap.containsKey(plz)) {
+            return zipCodeMap.get(plz);
+        } else{
+            throw new NoSuchElementException("No valid Zip-Code");
+        }
     }
 
     /**
@@ -47,18 +60,25 @@ public class ZipCode {
      * vorkommt. Falls der Ort nicht existiert wird eine leere Liste zurückgegeben.
      */
     LinkedList<String> getAllCantonsForName(String name) {
-
-        // Ihre Implementation
-        return null;
+        if (nameWithCantonsMap.containsKey(name)) {
+            return nameWithCantonsMap.get(name);
+        }else{
+            return new LinkedList<String>();
+        }
+        
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         File fileObject = new File("uebung011/src/test/resources/Adressdaten.csv");
         try {
-            new ZipCode(fileObject);
-        } catch (Exception e) {
-            throw e;
+            ZipCode zippo = new ZipCode(fileObject);
+            System.out.println(zippo.getNameForZipCode(5610));
+            System.out.println(zippo.getAllCantonsForName("Abtwils"));
+        } catch (FileNotFoundException e) {
+            System.err.println(e.getMessage());
         }
+
+        
         
         
     }
